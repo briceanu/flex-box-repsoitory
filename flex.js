f67ef2ce75668622a03d4cf61e3981f71c2fca56
Briceanu <ursus_24@yahoo.com>
2022-07-15 18:08:42 +0300

    more modifications add

--- flex.js
+++ flex.js
@@ -1,2 +1,16 @@
-var items = document.querySelectorAll("items");
-items.style.backgroundColor = "red";
+let item = document.querySelector(".square1");
+item.addEventListener("click", function () {
+  item.style.backgroundColor = "red";
+  item.style.color = "yellow";
+});
+
+let item2 = document.querySelector(".square2");
+item2.addEventListener("click", function () {
+  item2.style.background = "yellow";
+  item2.style.color = "red";
+});
+
+// let item3 = document.querySelector(".square3");
+//   item3.addEventListener('click',function(){
+
+//   })
